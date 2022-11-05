7bcd4696e1c274aa8dd1957642df4ede4106b15d
daidanretsu <daidanretsu@outlook.jp>
2022-11-06 01:17:55 +0900

    Add PcapDevice

--- src/interface/hal.rs
+++ src/interface/hal.rs
@@ -7,37 +7,41 @@ pub trait RawEthernetDevice {
     where
         Self: 'a;
 
-    /// If phi is not busy, return token.
     fn transmit<'a>(&'a mut self) -> Option<Self::TxToken<'a>>;
 
-    /// If phi is not busy, return token.
     fn receive<'a>(&'a mut self) -> Option<Self::RxToken<'a>>;
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum DeviceError {
+    Device,
+    Function,
+}
+
 pub trait TxToken {
-    fn consume<F>(self, len: usize, f: F) -> Result<(), ()>
+    fn consume<F>(self, len: usize, f: F) -> Result<(), DeviceError>
     where
         F: FnOnce(&mut [u8]) -> Result<(), ()>;
 }
 
 pub trait RxToken {
-    fn consume<F>(self, f: F) -> Result<(), ()>
+    fn consume<F>(self, f: F) -> Result<(), DeviceError>
     where
         F: FnOnce(&[u8]) -> Result<(), ()>;
 }
 
 #[cfg(feature = "smoltcp")]
-pub mod smoltcp_wrapper {
-    use super::{RawEthernetDevice, RxToken, TxToken};
+pub mod smoltcp {
+    use super::{DeviceError, RawEthernetDevice, RxToken, TxToken};
 
-    pub struct SmolDeviceWrapper<D>
+    pub struct SmolDevice<D>
     where
         D: for<'a> smoltcp::phy::Device<'a>,
     {
         device: D,
     }
 
-    impl<D> From<D> for SmolDeviceWrapper<D>
+    impl<D> From<D> for SmolDevice<D>
     where
         D: for<'a> smoltcp::phy::Device<'a>,
     {
@@ -52,7 +56,7 @@ pub mod smoltcp_wrapper {
     where
         T: smoltcp::phy::TxToken,
     {
-        fn consume<F>(self, len: usize, f: F) -> Result<(), ()>
+        fn consume<F>(self, len: usize, f: F) -> Result<(), DeviceError>
         where
             F: FnOnce(&mut [u8]) -> Result<(), ()>,
         {
@@ -60,7 +64,7 @@ pub mod smoltcp_wrapper {
                 .consume(smoltcp::time::Instant::from_secs(0), len, |buf| {
                     f(buf).map_err(|_| smoltcp::Error::Illegal)
                 })
-                .map_err(|_| ())
+                .map_err(|_| DeviceError::Device)
         }
     }
 
@@ -70,7 +74,7 @@ pub mod smoltcp_wrapper {
     where
         T: smoltcp::phy::RxToken,
     {
-        fn consume<F>(self, f: F) -> Result<(), ()>
+        fn consume<F>(self, f: F) -> Result<(), DeviceError>
         where
             F: FnOnce(&[u8]) -> Result<(), ()>,
         {
@@ -78,11 +82,11 @@ pub mod smoltcp_wrapper {
                 .consume(smoltcp::time::Instant::from_secs(0), |buf| {
                     f(buf).map_err(|_| smoltcp::Error::Illegal)
                 })
-                .map_err(|_| ())
+                .map_err(|_| DeviceError::Device)
         }
     }
 
-    impl<D> RawEthernetDevice for SmolDeviceWrapper<D>
+    impl<D> RawEthernetDevice for SmolDevice<D>
     where
         D: for<'d> smoltcp::phy::Device<'d>,
     {
@@ -96,6 +100,90 @@ pub mod smoltcp_wrapper {
             self.device
                 .receive()
                 .map(|(token, _)| SmolRxTokenWrapper(token))
+        }
+    }
+}
+
+#[cfg(feature = "pcap")]
+#[cfg(windows)]
+pub mod pcap {
+    use pcap::{Active, Capture, Device, Packet};
+
+    use crate::frame::ETHERNET_FRAME_SIZE_WITHOUT_FCS;
+
+    use super::{DeviceError, RawEthernetDevice, RxToken, TxToken};
+
+    pub struct PcapDevice<'a> {
+        cap: Capture<Active>,
+        buf: &'a mut [u8],
+    }
+
+    impl<'a> PcapDevice<'a> {
+        pub fn new(timeout_ms: i32, buf: &'a mut [u8]) -> Self {
+            assert!(ETHERNET_FRAME_SIZE_WITHOUT_FCS <= buf.len());
+            let cap = pcap::Capture::from_device(Device::lookup().unwrap().unwrap())
+                .unwrap()
+                .promisc(true)
+                .immediate_mode(true)
+                .timeout(timeout_ms)
+                .open()
+                .unwrap()
+                .setnonblock()
+                .unwrap();
+            Self { cap, buf }
+        }
+    }
+
+    impl<'a> RawEthernetDevice for PcapDevice<'a> {
+        type TxToken<'b> = PcapTxToken<'b>
+    where
+        Self: 'b;
+
+        type RxToken<'b> = PcapRxToken<'b>
+    where
+        Self: 'b;
+
+        fn transmit<'b>(&'b mut self) -> Option<Self::TxToken<'b>> {
+            let Self { cap, buf } = self;
+            Some(PcapTxToken { cap, buf })
+        }
+
+        fn receive<'b>(&'b mut self) -> Option<Self::RxToken<'b>> {
+            match self.cap.next_packet() {
+                Ok(packet) => Some(PcapRxToken(packet)),
+                Err(_) => None,
+            }
+        }
+    }
+
+    pub struct PcapTxToken<'a> {
+        cap: &'a mut Capture<Active>,
+        buf: &'a mut [u8],
+    }
+
+    impl<'a> TxToken for PcapTxToken<'a> {
+        fn consume<F>(self, len: usize, f: F) -> Result<(), DeviceError>
+        where
+            F: FnOnce(&mut [u8]) -> Result<(), ()>,
+        {
+            f(&mut self.buf[..len]).map_err(|_| DeviceError::Function)?;
+            self.cap
+                .sendpacket(&mut self.buf[..len])
+                .map_err(|_| ())
+                .map_err(|_| DeviceError::Device)?;
+            Ok(())
+        }
+    }
+
+    pub struct PcapRxToken<'a>(Packet<'a>);
+
+    impl<'a> RxToken for PcapRxToken<'a> {
+        fn consume<F>(self, f: F) -> Result<(), DeviceError>
+        where
+            F: FnOnce(&[u8]) -> Result<(), ()>,
+        {
+            let len = self.0.header.caplen as usize;
+            f(&self.0.data[..len]).map_err(|_| DeviceError::Function)
         }
     }
 }
